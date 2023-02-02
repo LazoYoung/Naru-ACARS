6253582ddaabc9503a38982b1e60d86c613ec6f9
lazoyoung <idealproduction@naver.com>
2023-02-02 15:15:59 +0900

    Include FSUIPC and Spring Boot

---
+++ src/main/java/com/naver/idealproduction/song/SONGApplication.java
@@ -0,0 +1,13 @@
+package com.naver.idealproduction.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SONGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SONGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naver/idealproduction/song/SimOverlayNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.idealproduction.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimOverlayNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
