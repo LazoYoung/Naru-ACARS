a96ca9fc0c1ee0a7ecde003cd390022ae3dfefc2
LazoYoung <idealproduction@naver.com>
2023-08-09 01:29:45 +0900

    fix: wrong socket uri

--- src/main/java/com/flylazo/naru_acars/domain/acars/VirtualAirline.java
+++ src/main/java/com/flylazo/naru_acars/domain/acars/VirtualAirline.java
@@ -3,8 +3,10 @@ package com.flylazo.naru_acars.domain.acars;
 import java.net.URI;
 
 public enum VirtualAirline {
+    // todo assign proper uri when ready
+    LOCAL("Localhost", URI.create("ws://127.0.0.1:6001/socket")),
     NARU_AIRLINE("Naru Airline", URI.create("")),
-    LOCAL("Localhost", URI.create("ws://127.0.0.1:6001"));
+    ;
 
     private final String text;
     private final URI uri;
